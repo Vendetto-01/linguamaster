48fb129e81ba4577009b58fd6859d10133748613
Süleyman Özcan <a.suleymanozcan@gmail.com>
2025-06-03 14:21:16 +0300

    backend fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,8 +2,8 @@ const config = {
   API_URL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000',
   ENDPOINTS: {
     WORDS: '/api/words',
-    DATABASE_INFO: '/api/database-info',
-    GENERATE_QUESTIONS: '/api/questions/generate',
+    DATABASE_INFO: '/api/questionWizard/database-info',
+    GENERATE_QUESTIONS: '/api/questionWizard/questions/generate',
   },
 };
 
